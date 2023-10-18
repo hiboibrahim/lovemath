bd417cde1a125f4f0200d110d2a57570e128a4e6
hibo ibrahim <hibo.ibrahim@gmail.com>
2023-10-18 18:56:58 +0000

    display addition questions to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,33 @@
-function runGame(){
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
 
+    
+    for (let  button of buttons){
+        button.addEventListener("click", function(){
+
+        if  (this.getAttribute("data-type") === "submit"){
+            alert("You clicked Submit!");
+        } else {
+            let gameType = this.getAttribute("data-type");
+            alert(`You Clicked $(gameType)`);
+        }
+
+        })
+    }
+    runGame("addition");
+})
+
+function runGame(gameType){
+    let num1 = Math.floor(Math.random() * 25) + 1; 
+    let num2 = Math.floor(Math.random() * 25) + 1;
+
+    if (gameType = "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else ('unknown game type: ${gameType}');
+    throw 'unknown game type: ${gameType}. Aborting!';
 }
 function checkAnswer(){
+
 
 }
 function calculateCorrectAnswer(){
@@ -14,7 +40,11 @@ function incrementWrongAnswer(){
 
 }
 
-function displayAdditionQuestion() {
+function displayAdditionQuestion(operand1, operand2) {
+    document.getElementById('operand1').textCOntent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = +;
+
 
 }
 function displaySubtractQuestion(){
